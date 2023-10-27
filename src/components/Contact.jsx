c5d3cf4bb1640697155af554a5bcc5f08f254641
saumyasaxenaa <saumyasaxena004@gmail.com>
2023-10-27 10:36:50 -0700

    Completed Email Functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,9 +17,44 @@ const Contact = () => {
   })
   const [loading, setLoading] = useState(false)
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  const handleSubmit = (e) => {}
+    setForm({ ...form, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setLoading(true)
+    emailjs.send(
+      'service_tkmxp2a',
+      'template_l1fqog7',
+      {
+        from_name: form.name,
+        to_name: 'Saumya',
+        from_email: form.email,
+        to_emai: 'saumyasaxena004@gmail.com',
+        message: form.message
+      },
+      'bh2_Rdk79RNFwhHYO'
+      )
+      .then(() => {
+        setLoading(false)
+        alert('Thank you. I will get back to you as soon as possible')
+
+        setForm({
+          name: '',
+          email: '',
+          message: ''
+        })
+      }, (error) => {
+        setLoading(false)
+
+        console.log(error)
+
+        alert('Something went wrong.')
+      })
+  }
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
